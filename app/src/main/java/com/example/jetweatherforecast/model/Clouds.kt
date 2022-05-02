037b7b3e76355af2669e26861d5579011a6920d7
sumitkahar992 <sumitkahar073@gmail.com>
2022-05-02 10:46:57 +0545

    Initial commit

---
+++ app/src/main/java/com/example/jetweatherforecast/model/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.jetweatherforecast.model
+
+data class Clouds(
+    val all: Int
+)
---
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.example.jetweatherforecast.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController){
+    Text(text = "main screen")
+}
